5f01179edd496a31df62493a3b51e14a5adfbdc1
Sumayyah Perine <sperine01@gmail.com>
2015-12-29 23:49:43 -0500

    edit comments controller, edit views template

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(comment_params)
+		flash[:notice] = "The comment was added"
+
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+
+	  	flash[:notice] = "The comment was removed"
+	  	redirect_to post_path(@post)
+	end
+
+private
+
+	def comment_params
+		params.require(:comment).permit(:namee, :body)
+	end
+
 end
